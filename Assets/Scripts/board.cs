0c687f646cbee7d4261dadb47f00759d07cd6871
SHIBINU001 <000928128@mohawkcollege.ca>
2025-02-01 12:26:34 -0500

    add piece script in unity

--- Assets/Scripts/board.cs
+++ Assets/Scripts/board.cs
@@ -2,5 +2,32 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 public class Board : MonoBehaviour
 {
+    public Tilemap tilemap { get; private set; }
     public TetrominoData[] tetrominoes;
+
+
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        for (int i = 0; i < tetrominoes.Length; i++)
+        {
+            this.tetrominoes[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, tetrominoes.Length);
+        TetrominoData data = tetrominoes[random];
+
+    }
+    public void Set()
+    {
+
+    }
 }
