7703531bd4b45b0e847e175935b35f26d34631d5
SHIBINU001 <000928128@mohawkcollege.ca>
2025-02-01 13:56:09 -0500

    setup the piece.cs , and put vector data to the piece.cs

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class piece : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using UnityEngine;
 
-    // Update is called once per frame
-    void Update()
+public class Piece : MonoBehaviour
+{
+    public Board board { get; private set; }
+    public TetrominoData data { get; private set; }
+    public Vector3Int[] cells { get; private set; }
+    public Vector3Int position { get; private set; }
+
+    public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
-        
+        this.board = board;
+        this.position = position;
+        this.data = data;
     }
 }
