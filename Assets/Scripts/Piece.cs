b7717f80c744b637dc1524188c04eb3ec48f8ef0
SHIBINU001 <000928128@mohawkcollege.ca>
2025-02-09 13:41:43 -0500

    Finished steps & locks. lock delay set 0.5f and step delay set 1f. if wanna game be harder , can set step delay smaller than 1f

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -8,12 +8,20 @@ public class Piece : MonoBehaviour
     public Vector3Int position { get; private set; }
     public int rotationIndex { get; private set; }
 
+    public float stepDelay = 1f;
+    public float lockDelay = 0.5f;
+    
+    private float stepTime;
+    private float lockTime;
+
     public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
         this.board = board;
         this.position = position;
         this.data = data;
         rotationIndex = 0;
+        this.stepTime = Time.time + this.stepDelay;
+        this.lockTime = 0f;
 
         if (this.cells == null)
         {
@@ -28,6 +36,7 @@ public class Piece : MonoBehaviour
     private void Update()
     {
         this.board.Clear(this);
+        this.lockTime += Time.deltaTime;
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
@@ -55,16 +64,38 @@ public class Piece : MonoBehaviour
         {
             HardDrop();
         }
-
+        if (Time.time > stepTime)
+        {
+            Step();
+        }
         this.board.Set(this);
     }
+
+    private void Step()
+    {
+        this.stepTime = Time.time + this.stepDelay;
+        Move(Vector2Int.down);
+
+        if (this.lockTime >= this.lockDelay)
+        {
+            Lock();
+        }
+    }
+
+    private void Lock()
+    {
+        board.Set(this);
+        board.SpawnPiece();
+    }
+
+
     private void HardDrop()
     {
         while (Move(Vector2Int.down))
         {
             continue;
         }
-
+        Lock();
     }
     private bool Move(Vector2Int translation)
     {
@@ -77,6 +108,7 @@ public class Piece : MonoBehaviour
         if (valid)
         {
             this.position = newPosition;
+            this.lockTime = 0f;
         }
         
         return valid;
