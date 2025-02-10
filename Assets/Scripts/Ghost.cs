1f5bee0ccf376e46eb4294146e362c1dc92f5d5a
SHIBINU001 <000928128@mohawkcollege.ca>
2025-02-09 19:17:19 -0500

    Finished Ghost Piece

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,18 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Ghost : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Tile tile;
+    public Board board;
+    public Piece trackingPiece;
+
+    public Tilemap tilemap { get; private set; }
+    public Vector3Int[] cells { get; private set; }
+    public Vector3Int position { get; private set; }
+
+    private void Awake()
     {
-        
+        tilemap = GetComponentInChildren<Tilemap>();
+        cells = new Vector3Int[4];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        Clear();
+        Copy();
+        Drop();
+        Set();
     }
+
+    private void Clear()
+    {
+        for (int i = 0; i < cells.Length; i++)
+        {
+            Vector3Int tilePosition = cells[i] + position;
+            tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    private void Copy()
+    {
+        for (int i = 0; i < this.cells.Length; i++) {
+            cells[i] = this.trackingPiece.cells[i];
+        }
+    }
+
+    private void Drop()
+    {
+        Vector3Int position = this.trackingPiece.position;
+
+        int current = position.y;
+        int bottom = -this.board.boardSize.y / 2 - 1;
+
+        this.board.Clear(this.trackingPiece);
+
+        for (int row = current; row >= bottom; row--)
+        {
+            position.y = row;
+
+            if (this.board.IsValidPosition(this.trackingPiece, position)) {
+                this.position = position;
+            } else {
+                break;
+            }
+        }
+
+        this.board.Set(trackingPiece);
+    }
+
+    private void Set()
+    {
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+            Vector3Int tilePosition = this.cells[i] + this.position;
+            this.tilemap.SetTile(tilePosition, tile);
+        }
+    }
+
 }
+
